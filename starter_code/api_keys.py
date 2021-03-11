ddb9762a001b496780df08992c64e02a6445613e
Jorge Ignacio Chavez Jr <cooktherook94@jorges-mbp.home>
2021-03-11 13:17:38 -0800

    updated homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "d1f1584a43eee974e37ce50cde3ab224"
+api_key = "d1f1584a43eee974e37ce50cde3ab224"
 
 # Google API Key
 g_key = "AIzaSyC5DBdhtr5-0UKYE08Pt7rBvxFrxcZH2X0"
